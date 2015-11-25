231a9d14e79d213adf170349254ef014b3f65da3
Remigijus <achmudass@gmail.com>
2015-11-25 08:16:31 +0200

    Started implementing deletion and addition of new feeds in frontend

--- src/main/resources/html/assets/js/application.js
+++ src/main/resources/html/assets/js/application.js
@@ -1,8 +1,27 @@
  var readItApp = angular.module('readItApp', []);
 
 
- readItApp.controller('FeedCtrl', function($scope){
-    $scope.feeds = [
+ readItApp.controller('FeedAddCtrl', function($scope, $rootScope) {
+
+     $scope.submit = function() {
+         if ($scope.feedUrl)
+            $rootScope.feeds.push({'name': $scope.feedUrl, 'numberOfNew':0});
+     };
+
+ });
+
+ readItApp.controller('FeedDeleteCtrl', function($scope, $rootScope) {
+
+     $scope.submit = function() {
+         $rootScope.feeds.splice($rootScope.feeds.indexOf($scope.feedToDelete), 1); //TODO prefill dropdown
+                                                                                    // TODO delete somehow from dropdown
+     };
+
+ });
+
+
+ readItApp.controller('FeedCtrl', function($rootScope){ // TODO change later to the scope and store value from backend
+    $rootScope.feeds = [
         {'name': 'levels.io',
             'numberOfNew': '1'},
         {'name': 'hacker.news',
@@ -59,3 +78,4 @@
             'numberOfNew': '54'}
     ]
  });
+
